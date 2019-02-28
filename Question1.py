104e199df9a799182208f94475f071db5106acde
sarahshiraz <48074714+sarahshiraz@users.noreply.github.com>
2019-02-27 20:23:16 -0500

    Removed commented lines

--- Question1.py
+++ Question1.py
@@ -28,31 +28,15 @@ def minibatch(x_train, y_train, w, b, num_epochs, digit):
 		x_shuffled = x_train[shuffled_indices]
 		y_shuffled = y_train[shuffled_indices]
 
-		# print("x_train_unrolledT")
-		# print(x_train.T.shape)
-
-		# print("w shape")
-		# print(w.shape)
 		z = np.dot(w,x_train.T) + b
-		# print("z shape")
-		# print(z.shape)
+
 		pred= 1/(np.exp(-z)+1)
 		pred[pred >= 0.7] = 1
 		pred[pred<0.7] = 0
-
-		# print("pred shape")
-		# print(pred.shape)
-
 		dist = np.square((y_train - pred)).tolist()
-		# print("dist shape")
-		# print(len(dist))
-
-
-		# print("len(dist)")
-		# print(len(dist))
-		acc = dist.count(0)/len(dist);
+		accuracy = dist.count(0)/len(dist);
 		cost = cost_function_sq_error(pred,y_train)
-		print("Classifer for digit: "+str(digit)+",		Epoch: " +str(num_epoch)+",     Accuracy: "+str(acc) +",     Cost: "+str(cost))
+		print("Classifer for digit: "+str(digit)+",		Epoch: " +str(num_epoch)+",     Accuracy: "+str(accuracy) +",     Cost: "+str(cost))
 		for i in range(0, m, batch_size):
 			xi = x_shuffled[i:i+batch_size]
 			yi = y_shuffled[i:i+batch_size]
@@ -67,13 +51,7 @@ def minibatch(x_train, y_train, w, b, num_epochs, digit):
 				grad = temp[j]*pred[j]*xi[j]
 				w = w - alpha*grad
 				b = b - (temp[j]*pred[j])
-			# gradient = np.dot(xi.T,temp1.T)
-			# w = w - alpha*gradient
-			# # print("w inside")
-			# # print(w.shape)
-			# b = b - (alpha*np.sum(pred - yi))
-
-	return w,b,acc
+	return w,b,accuracy
 
 
 
@@ -96,14 +74,15 @@ for x in x_test:
 x_test_unrolled = np.array(x_test_unrolled)
 x_test_unrolled = x_test_unrolled.astype('float32')
 x_test_unrolled/=255
-acc = np.zeros(10)
+accuracy = np.zeros(10)
 for i in range(10):
 	y_train = get_labels(i)
-	w[i,:], b[i], acc[i] = minibatch(x_train_unrolled, y_train, w[i,:], b[i], 5, i)
+	w[i,:], b[i], accuracy[i] = minibatch(x_train_unrolled, y_train, w[i,:], b[i], 5, i)
 
 
 for j in range(10):
-	print("Training Accuracy for classifier " + str(j) +": " + str(acc[j]))
+	print("Training Accuracy for classifier " + str(j) +": " + str(accuracy[j]))
+
 
 '''Testing'''
 x_test_unrolledT = x_test_unrolled.T
@@ -118,7 +97,6 @@ pred_all[pred_all >= 0.7] = 1
 pred_all[pred_all < 0.7] = 0
 pred = np.argmax(pred_all , axis = 0)
 dist = np.square((y_test - pred)).tolist()
-acc = dist.count(0)/len(dist);
+accuracy = dist.count(0)/len(dist);
 cost = cost_function_sq_error(pred,y_test)
-print( "Test Accuracy: "+str(acc) +",     Cost: "+str(cost))
-
+print( "Test Accuracy: "+str(accuracy) +",     Cost: "+str(cost))
